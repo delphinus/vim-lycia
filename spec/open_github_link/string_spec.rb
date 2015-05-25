43948a0657e4dc41593ec1279ace5bcb8a0d2023
delphinus <delphinus@remora.cx>
2015-05-26 00:34:20 +0900

    String#undent spec

--- spec/open_github_link/string_spec.rb
+++ spec/open_github_link/string_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe String, 'helper methods' do
   it { is_expected.to respond_to :undent }
   it { is_expected.to respond_to :red }
   it { is_expected.to respond_to :cyan }
+
+  describe '#undent' do
+
+    context 'when some indented text' do
+
+      let(:input) { <<-EOT }
+        some
+          indented
+            text
+
+          foo
+            bar
+      EOT
+      let(:expected) { <<-EOT }
+some
+  indented
+    text
+
+  foo
+    bar
+      EOT
+
+      it 'returns a valid undented text' do
+        expect(input.undent).to eq expected
+      end
+    end
+  end
 end
