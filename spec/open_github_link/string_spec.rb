36c72ac83bb3d346572b653a697f957dec26e3c5
delphinus <delphinus@remora.cx>
2015-05-26 00:57:24 +0900

    :red :cyan spec

--- spec/open_github_link/string_spec.rb
+++ spec/open_github_link/string_spec.rb
@@ -34,4 +34,46 @@ some
       end
     end
   end
+
+  describe 'colorish methods' do
+
+    let(:txt) { 'hoge' }
+    let(:colored_text) { "\033[#{color_code}m#{txt}\033[0m" }
+
+    before do
+      allow(STDOUT).to receive(:isatty).and_return isatty
+    end
+
+    shared_examples_for STDOUT: :isatty do
+
+      let(:isatty) { true }
+
+      it { is_expected.to eq colored_text }
+    end
+
+    shared_examples_for STDOUT: :not_isatty do
+
+      let(:isatty) { false }
+
+      it { is_expected.to eq txt }
+    end
+
+    describe '#red' do
+
+      let(:color_code) { 31 }
+      subject { txt.red }
+
+      context 'when isatty',     STDOUT: :isatty
+      context 'when not isatty', STDOUT: :not_isatty
+    end
+
+    describe '#cyan' do
+
+      let(:color_code) { 36 }
+      subject { txt.cyan }
+
+      context 'when isatty',     STDOUT: :isatty
+      context 'when not isatty', STDOUT: :not_isatty
+    end
+  end
 end
